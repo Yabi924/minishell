26f052abd3e7762d849c7147e56ad39a16835bdb
Yong Yan Bin <yyan-bin@u91z04s04.42kl.edu.my>
2024-11-20 20:54:48 +0800

    add note -> CTRL

--- include/minishell.h
+++ include/minishell.h
@@ -3,12 +3,10 @@
 
 # include "../libft/libft.h"
 # include "../libft/ft_printf.h"
-# include <stdio.h>
-# include <unistd.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <signal.h>
 
 void    input_handler(const char *input, char **env);
-// char *readline(const char *prompt);
 
 #endif
