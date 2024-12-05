a25633712c0605cb5027cca3c1c4179d282d792c
Yong Yan Bin <yyan-bin@u80z04s06.42kl.edu.my>
2024-12-05 17:01:33 +0800

    makefile

--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -6,35 +6,11 @@
 /*   By: yyan-bin <yyan-bin@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 20:02:57 by yyan-bin          #+#    #+#             */
-/*   Updated: 2024/12/03 20:02:58 by yyan-bin         ###   ########.fr       */
+/*   Updated: 2024/12/05 16:59:58 by yyan-bin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-char *readline(const char *prompt)
-{
-    printf("%s", prompt); // Display the prompt
-    (void)prompt;
-    size_t bufsize = 128;
-    char *buffer = malloc(bufsize);
-    if (!buffer) {
-        perror("Unable to allocate buffer");
-        return NULL;
-    }
-
-    if (fgets(buffer, bufsize, stdin) == NULL) {
-        free(buffer);
-        return NULL; // Handle EOF
-    }
-
-    size_t len = strlen(buffer);
-    if (buffer[len - 1] == '\n') {
-        buffer[len - 1] = '\0'; // Remove trailing newline
-    }
-
-    return buffer; // Caller must free this memory
-}
 
 void    input_handler(const char *input, char **env)
 {
