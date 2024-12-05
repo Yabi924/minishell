87a953146281b6c0562dba5347910afa7122fdc2
Wan Ilhan Idriss Wan Ab Kadir <wwan-ab-@u80z04s04.42kl.edu.my>
2024-12-05 16:50:35 +0800

    第一天. 我不喜欢 C

--- include/minishell.h
+++ include/minishell.h
@@ -52,4 +52,23 @@ char    *copy_input(char *input);
 void    input_handler(const char *input, char **env);
 
 
+/*
+    Coder: Idriss
+    Start date: 5/12/2024
+    *****This is my part, do not touch anything!*****
+*/
+
+//Structure for storing cmd history
+typedef struct CommandHistory {
+    char *cmd;
+    struct CommandHistory *next;
+} CommandHistory;
+
+//Define a structure for history management
+typedef struct history {
+    CommandHistory *head;
+} history;
+
+
+
 #endif
