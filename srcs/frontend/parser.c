7b794126245d6e97c6615c3aae1a8937f11e1a91
Yong Yan Bin <yyan-bin@u91z04s03.42kl.edu.my>
2025-02-14 15:42:32 +0800

    norm

--- srcs/frontend/parser.c
+++ srcs/frontend/parser.c
@@ -6,18 +6,16 @@
 /*   By: yyan-bin <yyan-bin@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 21:55:18 by yyan-bin          #+#    #+#             */
-/*   Updated: 2025/02/11 17:22:38 by yyan-bin         ###   ########.fr       */
+/*   Updated: 2025/02/14 14:55:46 by yyan-bin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void    parser(t_data *data)
+void	parser(t_data *data)
 {
-    add_space(data);
-    if (ft_strchr(data->new_input, '$'))
-        dollar_sign(data);
-    // ft_printf("%s\n", data->new_input);
-    tokenization(data, &data->list);
-    // pll(data->list);
+	add_space(data);
+	if (ft_strchr(data->new_input, '$'))
+		dollar_sign(data);
+	tokenization(data, &data->list);
 }
--- srcs/frontend/utils/check.c
+++ srcs/frontend/utils/check.c
@@ -6,42 +6,42 @@
 /*   By: yyan-bin <yyan-bin@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/25 19:27:01 by yyan-bin          #+#    #+#             */
-/*   Updated: 2025/01/08 22:24:47 by yyan-bin         ###   ########.fr       */
+/*   Updated: 2025/02/14 15:15:34 by yyan-bin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/minishell.h"
 
-int check_quotes(char *s)
+int	check_quotes(char *s)
 {
-    int i;
-    int count;
-    int count2;
+	int	i;
+	int	count;
+	int	count2;
 
-    i = -1;
-    count = 0;
-    count2 = 0;
-    while (s[++i])
-    {
-        if (s[i] && s[i] == '"')
-            count++;
-        if (s[i] && s[i] == '\'')
-            count2++;
-    }
-    if (count % 2 != 0 || count2 % 2 != 0)
-        return (1);
-    return (0);
+	i = -1;
+	count = 0;
+	count2 = 0;
+	while (s[++i])
+	{
+		if (s[i] && s[i] == '"')
+			count++;
+		if (s[i] && s[i] == '\'')
+			count2++;
+	}
+	if (count % 2 != 0 || count2 % 2 != 0)
+		return (1);
+	return (0);
 }
 
-int check_redrct_pipe(char *s)
+int	check_redrct_pipe(char *s)
 {
-    int i;
+	int	i;
 
-    i = -1;
-    while (s[++i])
-    {
-        if (s[i] == '|' || s[i] == '<' || s[i] == '>')
-            return (1);
-    }
-    return (0);
+	i = -1;
+	while (s[++i])
+	{
+		if (s[i] == '|' || s[i] == '<' || s[i] == '>')
+			return (1);
+	}
+	return (0);
 }
