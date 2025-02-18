29c13049faf3fb4804d91a300a7871a8700337aa
yabi <hin050924@gmail.com>
2025-02-18 12:12:02 +0800

    del debug message

--- srcs/frontend/parser.c
+++ srcs/frontend/parser.c
@@ -6,7 +6,7 @@
 /*   By: yyan-bin <yyan-bin@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 21:55:18 by yyan-bin          #+#    #+#             */
-/*   Updated: 2025/02/17 21:51:08 by yyan-bin         ###   ########.fr       */
+/*   Updated: 2025/02/18 12:11:44 by yyan-bin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,6 @@ void	parser(t_data *data)
 	init_split(data);
 	data->command_arr = split(&data->split, data->new_input);
 	reset_split(&data->split);
-	print_arr(data->command_arr);
+	// print_arr(data->command_arr);
 	tokenization(data, &data->list);
 }
