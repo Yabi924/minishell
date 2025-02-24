4f3c40ec71058ca204429b1852160269c762e99d
yabi <hin050924@gmail.com>
2025-02-24 19:06:49 +0800

    leak from input handle

--- srcs/frontend/input_handle.c
+++ srcs/frontend/input_handle.c
@@ -6,7 +6,7 @@
 /*   By: yyan-bin <yyan-bin@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 20:02:44 by yyan-bin          #+#    #+#             */
-/*   Updated: 2025/02/21 18:02:17 by yyan-bin         ###   ########.fr       */
+/*   Updated: 2025/02/23 18:36:18 by yyan-bin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,16 +57,21 @@ int	input_handle(t_data *data)
 	print_prompt(data);
 	skip_unprint(data);
 	if (lexer(data->input))
+	{
+		free(data->input);
+		data->input = NULL;
+		data->cmd_exit_no = 127;
 		ft_putstr_fd("Syntax error\n", 2);
+	}
 	else
 	{
 		parser(data);
 		// built_in(data, data->list); //Launch the built-ins and non built-ins (execve)
 		if (redirection(data, data->list))
 			ft_execute(data, data->list);
+		// tcsetattr(STDIN_FILENO, TCSANOW, &data->ori_terminal);
+		free_data(data);
 	}
-	tcsetattr(STDIN_FILENO, TCSANOW, &data->ori_terminal);
-	free_data(data);
 	return (0);
 }
 
