0488700914e077910bb813d145e466403ebe2ea8
wanpc4 <idrissilhan@gmail.com>
2025-02-24 15:17:43 +0800

    solve conflict

--- srcs/execute/execute.c
+++ srcs/execute/execute.c
@@ -11,11 +11,7 @@
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-#include "../../include/minishell.h"
 
-void	transit(t_data *data, t_list *list, pid_t *children)
-{
-	int	i;
 void	transit(t_data *data, t_list *list, pid_t *children)
 {
 	int	i;
@@ -108,73 +104,3 @@ void    ft_execute(t_data *data, t_list *list)
     unlink(".tmp");
     free(children);
 }
-
-/*
-static char *find_command(char *cmd, char **env)
-{
-    char *path_env = NULL;
-    char **paths = NULL;
-    char *full_path = NULL;
-    int i;
-
-    // Get the PATH variable from the environment
-    for (i = 0; env[i]; i++) {
-        if (strncmp(env[i], "PATH=", 5) == 0) {
-            path_env = env[i] + 5;
-            break;
-        }
-    }
-
-    if (!path_env) // No PATH in environment
-        return NULL;
-
-    // Split PATH into directories
-    paths = ft_split(path_env, ':');
-    if (!paths)
-        return NULL;
-
-    // Search for the command in each directory
-    for (i = 0; paths[i]; i++) {
-        full_path = ft_strjoin(paths[i], "/");
-        if (!full_path)
-            break;
-        full_path = ft_strjoin_free(full_path, cmd); // Append command to directory
-        if (!full_path)
-            break;
-        if (access(full_path, F_OK | X_OK) == 0) { // Command found
-            free(paths); // Free paths array
-            return full_path;
-        }
-        free(full_path);
-    }
-
-    free(paths); // Free paths array
-    return NULL; // Command not found
-}
-
-void    ft_execute(t_data *data, t_list *list)
-{
-    pid_t   p_id;
-    char    *cmd_path = find_command(list->command[0], data->env);
-
-    if (!cmd_path) {
-        ft_putstr_fd("Minidata: ", 2);
-        perror(list->command[0]);
-        data->cmd_exit_no = 127;
-        return;
-    }
-
-    p_id = fork();
-    if (p_id == 0) { // Child process
-        execve(cmd_path, list->command, data->env);
-        perror("execve"); // If execve fails
-        exit(1);
-    } else if (p_id > 0) { // Parent process
-        waitpid(p_id, &data->cmd_exit_no, 0);
-        data->cmd_exit_no = WEXITSTATUS(data->cmd_exit_no);
-    } else {
-        perror("fork");
-    }
-    free(cmd_path);
-}
-*/
